b460a12041ef8d486e8f58c918d7d7331ecb595c
JHEEE116 <sjhyt0813@gmail.com>
2025-06-03 03:11:02 +0900

    SwaggerRecovery

--- src/main/java/project/ec2session/domain/user/api/AuthApi.java
+++ src/main/java/project/ec2session/domain/user/api/AuthApi.java
@@ -58,13 +58,21 @@ public interface AuthApi {
                                         "username": "아이디는 필수 입력 값입니다."
                                     }
                                     """
-                            ),//name 공백일 경우 ui 출력X
+                            )//name 공백일 경우 ui 출력X
+                    })),
+
+            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+                    content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject (name = "이메일 형식 오류", value = """ 
                                     {
                                         "username": "올바른 이메일 형식이어야 합니다."
                                     }
                                     """
-                            ),
+                            )
+                    })),
+
+            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+                    content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject (name = "비밀번호 형식 오류", value = """
                                     {
                                         "password": "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함"

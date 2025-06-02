2a657f47768456e6232c5127d18b148375cdd49a
JHEEE116 <sjhyt0813@gmail.com>
2025-06-03 03:16:10 +0900

    recovery

--- src/main/java/project/ec2session/domain/user/api/AuthApi.java
+++ src/main/java/project/ec2session/domain/user/api/AuthApi.java
@@ -48,38 +48,62 @@ public interface AuthApi {
                                     }
                                     """)
                     })),
-            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "회원가입 유효성 검사 실패",
                     content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject(name = "회원가입 유효성 검사 실패",
-                                    value = """
-                                    {
-                                        "password": "비밀번호는 필수 입력 값입니다.",
-                                        "nickname": "닉네임은 필수 입력 값입니다.",
-                                        "username": "아이디는 필수 입력 값입니다."
-                                    }
-                                    """
-                            )//name 공백일 경우 ui 출력X
-                    })),
+                            @ExampleObject(name = "필수 항목 누락", value = """
+                    {
+                        "password": "비밀번호는 필수 입력 값입니다.",
+                        "nickname": "닉네임은 필수 입력 값입니다.",
+                        "username": "아이디는 필수 입력 값입니다."
+                    }
+                    """),
+                            @ExampleObject(name = "이메일 형식 오류", value = """
+                    {
+                        "username": "올바른 이메일 형식이어야 합니다."
+                    }
+                    """),
+                            @ExampleObject(name = "비밀번호 형식 오류", value = """
+                    {
+                        "password": "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함"
+                    }
+                    """)
+                    })
+            )
 
-            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
-                    content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject (name = "이메일 형식 오류", value = """ 
-                                    {
-                                        "username": "올바른 이메일 형식이어야 합니다."
-                                    }
-                                    """
-                            )
-                    })),
-
-            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
-                    content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject (name = "비밀번호 형식 오류", value = """
-                                    {
-                                        "password": "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함"
-                                    }
-                                    """
-                            )
-                    }))
+//            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+//                    content = @Content(mediaType = "application/json", examples = {
+//                            @ExampleObject(name = "회원가입 유효성 검사 실패",
+//                                    value = """
+//                                    {
+//                                        "password": "비밀번호는 필수 입력 값입니다.",
+//                                        "nickname": "닉네임은 필수 입력 값입니다.",
+//                                        "username": "아이디는 필수 입력 값입니다."
+//                                    }
+//                                    """
+//                            )//name 공백일 경우 ui 출력X
+//                    })),
+//
+//            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+//                    content = @Content(mediaType = "application/json", examples = {
+//                            @ExampleObject (name = "이메일 형식 오류", value = """
+//                                    {
+//                                        "username": "올바른 이메일 형식이어야 합니다."
+//                                    }
+//                                    """
+//                            )
+//                    })),
+//
+//            @ApiResponse(responseCode = "400", description = "회원가입 유효성 검사 실패",
+//                    content = @Content(mediaType = "application/json", examples = {
+//                            @ExampleObject (name = "비밀번호 형식 오류", value = """
+//                                    {
+//                                        "password": "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함"
+//                                    }
+//                                    """
+//                            )
+//                    }))
     })
     ResponseEntity<?> signUp(@RequestBody @Valid UserReq.SignUpDto request);
 }
